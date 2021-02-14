05b426064c388a0de7062d3027b28f68c3cc0867
dyl25 <dylan-vst@hotmail.com>
2021-02-14 10:13:38 +0100

    add configuration for  jwt token auth

---
+++ app/Http/Controllers/Api/ExerciseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Exercise;
+use Illuminate\Http\Request;
+
+class ExerciseController extends Controller
+{
+    public function index() {
+        return Exercise::all();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ExerciseController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/exercises', [ExerciseController::class, 'index']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::post('/me', [AuthController::class, 'me']);
