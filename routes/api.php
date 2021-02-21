d2bd533faab7d899da06286c512e36f310059f25
dyl25 <dylan-vst@hotmail.com>
2021-02-21 18:07:17 +0100

    configure jwt auth token

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/exercises', [ExerciseController::class, 'index']);
+Route::middleware('auth:api')->group(function () {
+    Route::get('/exercises', [ExerciseController::class, 'index']);
+
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
